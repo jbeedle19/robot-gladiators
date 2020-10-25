c25d11c28fc7ccd6d2c07a5006582f362217cfc7
jbeedle19 <josh.beedle@gmail.com>
2020-10-25 18:51:33 -0400

    tested different functions and logs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+function fight() {
+   window.alert("The fight has begun!");
+}
+/* this creates a variable called playerName and will store
+the data that the player enters as their name */
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10+10);
+//what is this?
+console.log("Our robot's name is " + playerName);
+// fight();
