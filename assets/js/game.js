e992252c43dc0002b912e563d10f4c24fc6a27a0
jbeedle19 <josh.beedle@gmail.com>
2020-10-26 18:33:08 -0400

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,34 +1,22 @@
-// Creating variables for player's Name, Health, and Attack
-//var playerName = window.prompt("What is your robot's name?");
-var playerHealth = 100;
-var playerAttack = 10;
-var playerMoney = 10;
-
-// You can also log multiple values at once like this
-//console.log(playerName, playerAttack, playerHealth, playerMoney);
-
-// Creating variables for enemy Robot
-var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-console.log(enemyNames);
-console.log(enemyNames[0]);
-console.log(enemyNames[1]);
-console.log(enemyNames[2]);
-console.log(enemyNames.length - 1);
-for(var i = 0; i < enemyNames.length; i++) {
-    console.log(enemyNames[i]);
-    console.log(i);
-    console.log(enemyNames[i] + " is at " + i + " index");
-}
-var enemyHealth = 50;
-var enemyAttack = 12;
-
 // Game States
 // "WIN" - Player robot has defeated all enemy-robots
 //      * Fight all enemy-robots
 //      * Defeat each enemy-robot
 // "LOSE" - Player robot's health is zero or less
 
-var fight = function() {
+// Creating variables for player's Name, Health, and Attack
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+var playerMoney = 10;
+
+
+// Creating variables for enemy Robot
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -85,4 +73,7 @@ var fight = function() {
     }
 }
 }
-//fight();
+
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i])
+}
