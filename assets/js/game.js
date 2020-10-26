6d7964e21b18291a74b40c3495d6b58d1d3c63da
jbeedle19 <josh.beedle@gmail.com>
2020-10-26 18:05:33 -0400

    added an array for enemy robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,27 @@
 // Creating variables for player's Name, Health, and Attack
-var playerName = window.prompt("What is your robot's name?");
+//var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
 // You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
+//console.log(playerName, playerAttack, playerHealth, playerMoney);
 
 // Creating variables for enemy Robot
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames);
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames.length - 1);
 var enemyHealth = 50;
 var enemyAttack = 12;
+
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//      * Fight all enemy-robots
+//      * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
 
 var fight = function() {
     // Alert players that they are starting the round
@@ -69,4 +80,4 @@ var fight = function() {
     }
 }
 }
-fight();
+//fight();
